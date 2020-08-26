1edf27efa36917ed6f346656f24253972910fd72
Edinei de Menezes <edineidemenezes@Edineis-Mac.local>
2020-08-25 17:21:19 -0700

    Projeto apenas IPHONE

---
+++ IKeepassOne/ViewController.cs
@@ -0,0 +1,31 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+namespace IKeepassOne
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+            btnTeste.TouchUpInside += (object sender, EventArgs e) =>
+            {
+                var alert = UIAlertController.Create("TESTE", "Mensagem", UIAlertControllerStyle.Alert);
+                alert.AddAction(UIAlertAction.Create("OK", UIAlertActionStyle.Default, null));
+                PresentViewController(alert, true, null);
+            };
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
